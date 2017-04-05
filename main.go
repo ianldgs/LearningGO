4741bfff011a3efe563e648820ce991d05d6f742
Ian Luca <ian.naph@gmail.com>
2017-04-05 17:25:53 -0300

    melhores exemplos

--- main.go
+++ main.go
@@ -1,12 +1,36 @@
+
+// package/namespaces
 package main
 
+// import other packages
 import "fmt"
 
+// null
+nil
+
+// var
 var a, b, c = "a", "b", "c"
-var x, y, z = 1, 2, 3
+var x, y, z int
 
+// if
+if x == y
 
+// for
+for i := 0; i < count; i++ {}
+
+// while
+for i < 10 {}
+
+// private
+func do() {}
+
+// public
+func Do() {}
+
+// main
 func main() {
-	fmt.Println(a, b, c, x, y, z)
-	//true Hello World! 1 0 0 2.2 (-5+12i)
+	// type inference
+	i := 1 //int
+	s := "str" //string
 }
+
